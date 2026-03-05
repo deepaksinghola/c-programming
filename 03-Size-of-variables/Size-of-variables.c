162ca6a336db3172c2f1377d2241e902988832d9
Deepak Singh Ola <deepaksinghola@192.168.1.3>
2026-03-06 03:59:20 +0530

    Size of variables

---
+++ 03-Size-of-variables/Size-of-variables.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main() {
+
+    printf("The size of char variable is %lu\n",sizeof(char));
+    printf("The size of void variable is %lu\n",sizeof(void));
+    printf("The size of int variable is %lu\n",sizeof(int));
+    printf("The size of float variable is %lu\n",sizeof(float));
+    printf("The size of unsigned int variable is %lu\n",sizeof(unsigned int));
+    printf("The size of long int variable is %lu\n",sizeof(long int));
+    printf("The size of long long int variable is %lu\n",sizeof(long long int));
+    printf("The size of double variable is %lu\n",sizeof(double));
+    printf("The size of long double variable is %lu\n",sizeof(long double));
+    printf("The size of unsigned long variable is %lu\n",sizeof(unsigned long));
+
+
+    return 0;
+
+
+
+}
