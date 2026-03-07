504f98d9ab3fe32d3f02b53adcaccefd2c44c7ae
Deepak Singh Ola <deepaksinghola@192.168.1.3>
2026-03-07 07:45:24 +0530

    ASCII values

---
+++ 06-ASCII-values/ASCII-values.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+
+    char ch;
+    printf("Enter a character:");
+    scanf("%c",&ch);
+
+    printf("\nThe ASCII value of %c is %d\n\n",ch,ch);
+
+    return 0;
+
+}
